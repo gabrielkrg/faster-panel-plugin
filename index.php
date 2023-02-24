597c0872a676fa166530dadf0661722be8fe07dd
Gabriel Rodrigues <gabrielkrodrigues1@gmail.com>
2023-02-24 17:38:47 -0300

    add plugin status to wp admin bar

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ if (function_exists('acf_add_local_field_group')) {
                     'id' => '',
                 ),
                 'choices' => array(
-                    'active' => 'Activated',
-                    'deactive' => 'Deactivated',
+                    'active' => 'Ativado',
+                    'deactive' => 'Desativado',
                 ),
                 'default_value' => false,
                 'return_format' => 'value',
@@ -63,11 +63,56 @@ if (function_exists('acf_add_local_field_group')) {
 add_action('init', 'user_config');
 function user_config()
 {
+    add_action('admin_head', 'fpb_styles');
+    function fpb_styles()
+    {
+        wp_enqueue_style('fpb-styles', plugins_url('assets/css/styles.css', __FILE__));
+    }
+
     $user_id = get_current_user_id();
     $faster_panel = get_field('faster_panel_acf', 'user_' . $user_id);
 
     if (!$user_id || !$faster_panel) {
         return;
+    }
+
+    add_action('admin_bar_menu', 'admin_bar_item', 500);
+    function admin_bar_item(WP_Admin_Bar $admin_bar)
+    {
+        $user_id = get_current_user_id();
+        $faster_panel = get_field('faster_panel_acf', 'user_' . $user_id);
+        $title = "";
+
+        if (!current_user_can('manage_options')) {
+            return;
+        }
+
+        if ($faster_panel == 'active') {
+
+            $args = array(
+                'id'    => 'menu-fast-active',
+                'parent' => null,
+                'group'  => null,
+                'title' => 'Acelerador de painel - Ativado',
+                'href'  => get_edit_profile_url(),
+                'meta' => [
+                    'title' => __('Clique aqui para ir às configurações de usuário e alterar essa opção. Basta ir até a sessão User options > Acelerar painel.', 'textdomain'), //This title will show on hover
+                ]
+            );
+        } else {
+            $args = array(
+                'id'    => 'menu-fast-deactive',
+                'parent' => null,
+                'group'  => null,
+                'title' => 'Acelerador de painel - Desativado',
+                'href'  => get_edit_profile_url(),
+                'meta' => [
+                    'title' => __('Clique aqui para ir às configurações de usuário e alterar essa opção. Basta ir até a sessão User options > Acelerar painel.', 'textdomain'), //This title will show on hover
+                ]
+            );
+        }
+
+        $admin_bar->add_menu($args);
     }
 
     if (is_admin() && $faster_panel == 'active') {
